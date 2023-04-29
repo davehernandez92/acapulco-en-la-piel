eb7f93e76401487b7d4163acb7b379b3c712e112
davehernandez92 <davehgal@gmail.com>
2023-04-29 11:04:31 -0300

    fixed performance on youtube vid on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ import logo from "../../public/Acapulco-logo.png"
 import heroCSS from '../styles/hero.module.css'
 
 
-export default function Home({videoId}) {
+export default function Home() {
   
-  console.log(videoId.attributes.codigo);
+ 
   return (
     <>
       <Layout title={"Inicio"}>
@@ -28,27 +28,9 @@ export default function Home({videoId}) {
           ¡Tenemos justo lo que necesitas! Entra y conoce nuestro canal y
           descubre todo Acapulco en un solo lugar. Desde hoteles, restaurantes,
           playas y los mejores lugares que debes conocer."
-          video={`${videoId.attributes.codigo}`}
+          video={'03eVtzPaCo8'}
         />
       </Layout>
     </>
   );
 }
-export async function getServerSideProps(context) {
-  const { params } = context;
-
-  const response = await fetch(`${process.env.API_URL}/youtube`)
-  const {data}  = await response.json()
-
-  const videoId = data
-
-
-
-
-  return {
-    props: {
-      videoId
-    },
-  };
-}
-
